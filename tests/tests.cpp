a259435c02d08e8610dce7e305eb859f997094aa
Daniel Klock <daniel.a.klock@gmail.com>
2018-02-04 09:52:00 +0100

    Update tests to use set instead of vector.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,6 @@
 #include "prime.hpp"
 #include <cassert>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
@@ -9,18 +10,18 @@ int main() {
     const vector<unsigned> nonPrimes = {0, 1, 4, 6, 8, 9, 10, 12, 14, 15, 16,
                                         18, 20, 21, 22, 24, 25, 26, 27, 28, 30};
 
-    for (unsigned prime : primes) {
+    for (const unsigned prime : primes) {
         assert(isPrime(prime));
     }
 
-    for (unsigned nonPrime : nonPrimes) {
+    for (const unsigned nonPrime : nonPrimes) {
         assert(!isPrime(nonPrime));
     }
 
     std::mutex numMutex;
-    std::vector<unsigned> foundPrimes;
+    std::set<unsigned> foundPrimes;
     findPrimes(0, 30, foundPrimes, numMutex);
-    for (unsigned found : foundPrimes) {
+    for (const unsigned found : foundPrimes) {
         assert(find(primes.begin(), primes.end(), found) != primes.end());
         assert(find(nonPrimes.begin(), nonPrimes.end(), found) == nonPrimes.end());
     }
