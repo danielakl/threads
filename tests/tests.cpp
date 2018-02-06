09ed3702974f6e0e7d601b0a9ad857c0641193d3
Daniel Klock <daniel.a.klock@gmail.com>
2018-02-06 14:48:11 +0100

    Update tests to use unsigned long long int.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,24 +4,25 @@
 #include <algorithm>
 
 using namespace std;
+typedef unsigned long long int ulli;
 
 int main() {
-    const vector<unsigned> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-    const vector<unsigned> nonPrimes = {0, 1, 4, 6, 8, 9, 10, 12, 14, 15, 16,
+    const vector<ulli> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+    const vector<ulli> nonPrimes = {0, 1, 4, 6, 8, 9, 10, 12, 14, 15, 16,
                                         18, 20, 21, 22, 24, 25, 26, 27, 28, 30};
 
-    for (const unsigned prime : primes) {
+    for (const ulli prime : primes) {
         assert(isPrime(prime));
     }
 
-    for (const unsigned nonPrime : nonPrimes) {
+    for (const ulli nonPrime : nonPrimes) {
         assert(!isPrime(nonPrime));
     }
 
     std::mutex numMutex;
-    std::set<unsigned> foundPrimes;
+    std::set<ulli> foundPrimes;
     findPrimes(0, 30, foundPrimes, numMutex);
-    for (const unsigned found : foundPrimes) {
+    for (const ulli found : foundPrimes) {
         assert(find(primes.begin(), primes.end(), found) != primes.end());
         assert(find(nonPrimes.begin(), nonPrimes.end(), found) == nonPrimes.end());
     }
